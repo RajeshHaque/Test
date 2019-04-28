36d7c1495a734f4a47a7d556bfc8da30bc40beef
RajeshHaque <rajeshmhaque@gmail.com>
2019-04-28 19:52:43 +0530

    my first project

---
+++ the great india.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>The Great India</title>
+	<style>
+		body{
+			background-color: whitesmoke;
+		}
+	</style>
+</head>
+<body>
+	<!--The Great Indian Website-->
+	<!--Main Navigation-->
+	<div>
+		<a href="#">Home</a>
+		<a href="#">Before & After</a>
+		<a href="#">Freedom Fighters</a>
+		<a href="#">Political Leaders</a>
+		<a href="#">Business Leaders</a>
+		<a href="#">Sports Leader</a>
+	</div>
+</body>
+</html>
